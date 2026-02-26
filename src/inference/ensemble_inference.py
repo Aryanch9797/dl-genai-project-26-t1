1d9701abad17110b769b8d74f3ef48f2a24c1800
Aryan Chauhan <aryanch9797@gmail.com>
2026-02-26 14:51:00 +0530

    added inference for multiple models

---
+++ src/inference/ensemble_inference.py
@@ -0,0 +1,56 @@
+import torch
+import pandas as pd
+from tqdm import tqdm
+
+def ensemble_and_submit(models, loader, label_map, device, AST_model=None, AST_loader=None, ):
+    """
+    Ensembles three models via logit averaging and creates submission file.
+    """
+    for model in models:
+        model.eval()
+    if AST_model is not None:
+        AST_model.eval()
+    
+    final_preds = []
+    ids = []
+    
+    test_ids = loader.dataset.test_csv.index.tolist()
+    print(f"Starting Ensemble on {device}...")
+
+    with torch.inference_mode():
+        for i, x in enumerate(tqdm(loader)):
+            x = x.squeeze(0) 
+            x = x.to(device)
+            
+
+            logits_cnn = models[0](x)
+            logits_resnet = models[1](x)
+
+            avg_logits_cnn = torch.mean(logits_cnn, dim=0)        # calculating mean for all chunks
+            avg_logits_resnet = torch.mean(logits_resnet, dim=0)
+
+            final_logits = (avg_logits_cnn + avg_logits_resnet ) / 2  # average all three models
+            
+            if AST_model is not None and AST_loader is not None:
+                ast_x = AST_loader.dataset[i][0].unsqueeze(0).to(device)  # Get AST input for the same index
+                logits_ast = AST_model(ast_x)
+                avg_logits_ast = torch.mean(logits_ast, dim=0)  # Average over chunks for AST
+
+                final_logits = (logits_cnn + logits_resnet + avg_logits_ast) / 3  # Average all three models
+            
+            pred_idx = torch.argmax(final_logits).item()
+            predicted_label = label_map[pred_idx]
+            
+            final_preds.append(predicted_label)
+            ids.append(test_ids[i])
+
+
+    submission = pd.DataFrame({
+        'id': ids,
+        'genre': final_preds
+    })
+    
+
+    submission.to_csv("submission.csv", index=False)
+    print("Ensemble complete. Saved to 'submission.csv'")
+    return submission
